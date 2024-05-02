9449263587304e36ea679d3a83a0a96a929b1cd5
Bhimji Patel <bhimjipatel@hotmail.com>
2024-05-02 17:29:48 +0530

    Initial commit

--- bin/static-cdk.ts
+++ bin/static-cdk.ts
@@ -1,21 +1,50 @@
-#!/usr/bin/env node
 import 'source-map-support/register';
 import * as cdk from 'aws-cdk-lib';
-import { StaticCdkStack } from '../lib/static-cdk-stack';
+import {StaticSiteStack} from '../lib/static-site.stack';
+import {identifyResource} from '../lib/config-util';
+import {CiCdStack} from '../lib/ci-cd.stack';
+import {Fn} from 'aws-cdk-lib';
+
+const accountId = '720620135416';
+const region = 'us-east-1';
 
 const app = new cdk.App();
-new StaticCdkStack(app, 'StaticCdkStack', {
-  /* If you don't specify 'env', this stack will be environment-agnostic.
-   * Account/Region-dependent features and context lookups will not work,
-   * but a single synthesized template can be deployed anywhere. */
 
-  /* Uncomment the next line to specialize this stack for the AWS Account
-   * and Region that are implied by the current CLI configuration. */
-  // env: { account: process.env.CDK_DEFAULT_ACCOUNT, region: process.env.CDK_DEFAULT_REGION },
+// Create stack that sets up the static hosting of the site
+const staticSiteResourcePrefix = 'cdk-web-static';
+const STATIC_SITE_BUCKET_NAME_OUTPUT_ID = identifyResource(staticSiteResourcePrefix, 'bucket-name');
+const STATIC_SITE_DISTRIBUTION_ID_OUTPUT_ID = identifyResource(staticSiteResourcePrefix, 'distribution-id');
 
-  /* Uncomment the next line if you know exactly what Account and Region you
-   * want to deploy the stack to. */
-  // env: { account: '123456789012', region: 'us-east-1' },
+new StaticSiteStack(app, identifyResource(staticSiteResourcePrefix, 'stack'), {
+  env: {
+    account: accountId,
+    region: region,
+  },
+  resourcePrefix: staticSiteResourcePrefix,
+  hostedZoneName: 'gler-app.click',
+  domainName: 'gler-app.click',
+  includeWWW: false,
+  siteSourcePath: '../dist',
+  staticSiteBucketNameOutputId: STATIC_SITE_BUCKET_NAME_OUTPUT_ID,
+  staticSiteDistributionIdOutputId: STATIC_SITE_DISTRIBUTION_ID_OUTPUT_ID,
+});
 
-  /* For more information, see https://docs.aws.amazon.com/cdk/latest/guide/environments.html */
+// Create stack that sets up the CI/CD pipeline for the static site
+const ciCdResourcePrefix = 'cdk-web-cicd';
+const staticSiteBucket = Fn.importValue(STATIC_SITE_BUCKET_NAME_OUTPUT_ID);
+const staticSiteDistributionId = Fn.importValue(STATIC_SITE_DISTRIBUTION_ID_OUTPUT_ID);
+
+new CiCdStack(app, identifyResource(ciCdResourcePrefix, 'stack'), {
+  env: {
+    account: accountId,
+    region: region
+  },
+  resourcePrefix: ciCdResourcePrefix,
+  distributionId: staticSiteDistributionId,
+  bucket: staticSiteBucket,
+  repo: 'static-cdk',
+  repoOwner: 'bhimjipatel',
+  repoBranch: 'master',
+  githubTokenSecretId: '/static-cdk/cicd/github_token',
+  buildAlertEmail: 'bhimjipatel@hotmail.com',
 });
