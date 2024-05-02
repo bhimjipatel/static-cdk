67daa108d4797750a40f9721712666fd0fb6a6c3
Bhimji Patel <bhimjipatel@hotmail.com>
2024-05-02 17:18:32 +0530

    Initial commit

---
+++ test/static-cdk.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as StaticCdk from '../lib/static-cdk-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/static-cdk-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new StaticCdk.StaticCdkStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
